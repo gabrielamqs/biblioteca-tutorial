848d56d5ce9e018d3f66048db96be5784f2b2523
gabrielamqs <gabriela.benevidespm@gmail.com>
2026-09-23 19:53:09 -0300

    corrige marcadores de conflito no EventBus

--- src/shared/EventBus.ts
+++ src/shared/EventBus.ts
@@ -13,20 +13,12 @@ export class EventBus {
   subscribe<E extends Event>(nome: E["nome"], listener: Listener<E>): void {
     const atuais = this.listeners.get(nome) ?? [];
 
-<<<<<<< HEAD
-    this.listeners.set(nome, [...atuais, listener as Listener<never>]);
-=======
     this.listeners.set(nome, [...atuais, listener]);
->>>>>>> f9e0124aa7bd1a555dfda973c734390690e669e7
   }
 
   publish<E extends Event>(event: E): void {
     for (const listener of this.listeners.get(event.nome) ?? []) {
-<<<<<<< HEAD
-      (listener as unknown as Listener<E>)(event);
-=======
       (listener as Listener<E>)(event);
->>>>>>> f9e0124aa7bd1a555dfda973c734390690e669e7
     }
   }
 }
