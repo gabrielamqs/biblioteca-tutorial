300b9b476ad5b5b605fafcf29649fc20aaca4fc3
Gabriela Benevides <147735318+gabrielamqs@users.noreply.github.com>
2026-09-24 17:12:41 -0300

    Update index.ts

--- src/modules/acervo/index.ts
+++ src/modules/acervo/index.ts
@@ -1,4 +1,5 @@
 export { CadastrarLivro } from "./features/cadastrar-livro/CadastrarLivro";
+export { RegistrarAvaliacao } from "./features/registrar-avaliacao/RegistrarAvaliacao";
 export { BuscarLivro } from "./features/buscar-livro/BuscarLivro";
 export { SqliteLivroRepository } from "./infrastructure/SqliteLivroRepository";
 export { createAcervoTables } from "./infrastructure/schema";
